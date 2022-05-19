bbfec4cc691712d64b0ef4ce8ebd03278a8db3bb
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-05-19 17:41:14 -0300

    FEAT: Create route and injetion depencies

---
+++ src/route/taskRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import TaskController from "../controller/taskController";
+import connection from "../db/connection";
+import TaskModel from "../model/taskModel";
+import TaskService from "../service/taskService";
+
+const taskRouter = Router();
+
+const taskModel = new TaskModel(connection);
+const taskService = new TaskService(taskModel);
+const taskController = new TaskController(taskService);
+
+taskRouter.get('/', taskController.findAll);
+
+export default taskRouter;
