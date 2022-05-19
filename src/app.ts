c8d69825f41a657d7bc4364fc63473ff251047dc
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-05-19 17:41:41 -0300

    Refactor: Add endpoint get/task

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import taskRouter from './route/taskRouter';
 
 export default class App {
   public app: express.Express
@@ -6,7 +7,11 @@ export default class App {
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.routes();
+  }
 
+  private routes() {
+    this.app.use('/task', taskRouter);
   }
   public start(PORT: string | number) {
     this.app.listen(PORT, () => console.log(`Listening on ${PORT}`));
