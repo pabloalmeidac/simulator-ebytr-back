f219eeeffc7766e75c8eeb413263d05a016fcca3
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-05-19 21:56:04 -0300

    FEAT: Created middleware validateBody add in route

--- src/route/taskRouter.ts
+++ src/route/taskRouter.ts
@@ -3,6 +3,9 @@ import connection from "../db/connection";
 import TaskController from "../controller/taskController";
 import TaskModel from "../model/taskModel";
 import TaskService from "../service/taskService";
+import createSchema from "../schema/create";
+import validateBody from "../middleware/validateBody";
+import editSchema from "../schema/edit";
 
 const taskModel = new TaskModel(connection);
 const taskService = new TaskService(taskModel);
@@ -11,8 +14,8 @@ const taskController = new TaskController(taskService);
 const taskRouter = Router();
 
 taskRouter.get('/', taskController.findAll);
-taskRouter.post('/', taskController.create);
-taskRouter.put('/:id', taskController.update);
+taskRouter.post('/', validateBody(createSchema), taskController.create);
+taskRouter.put('/:id', validateBody(editSchema), taskController.update);
 taskRouter.delete('/:id', taskController.remove);
 
 export default taskRouter;
