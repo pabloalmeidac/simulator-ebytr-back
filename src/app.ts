da02b98e09a5f3ac14562354177a494da7864be2
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-05-19 21:59:26 -0300

    FEAT: Add config cors

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,21 @@ export default class App {
 
   constructor() {
     this.app = express();
-    this.app.use(express.json());
+    this.config();
     this.routes();
     this.errorMiddleware();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+    this.app.use(express.json());
   }
 
   private routes() {
